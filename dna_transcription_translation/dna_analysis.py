3b2180022199549aca877165e98f032c94381ca6
Sanika Bhoir <bhoirsanikadevidas05@gmail.com>
2026-09-04 01:04:47 +0530

    Delete dna_transcription_translation directory

--- dna_transcription_translation/dna_analysis.py
+++
@@ -1,8 +0,0 @@
-from Bio.Seq import Seq
-dna = input("Enter DNA sequence: ").upper()
-dna_seq = Seq(dna)
-rna = dna_seq.transcribe()
-protein = dna_seq.translate()
-print("\nDNA:", dna)
-print("RNA:", rna)
-print("Protein:", protein)
